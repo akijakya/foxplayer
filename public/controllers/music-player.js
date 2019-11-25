143faa5b8ab88763b56d29d032695cc649e02de7
András Jáky <jakyandras@gmail.com>
2019-11-25 22:02:45 +0100

    implemented play/pause function

--- public/controllers/music-player.js
+++ public/controllers/music-player.js
@@ -1,19 +1,28 @@
 'use strict';
 
-let playButton = document.getElementsByClassName('play-button')[0];
 let audio = document.getElementsByTagName('audio')[0];
 
-// play/pause function
-playButton.addEventListener('click', function(e) {
-  if(audio.paused) {
-    audio.play();
-    playButton.src = './public/assets/img/pause.svg';
-  }
-  else {
-    audio.pause();
-    playButton.src = './public/assets/img/play.svg';
-  }
-}, false);
+// PLAY/PAUSE FUNCTION
+function playPause () {
+    if(audio.paused) {
+        audio.play();
+        playButton.src = './public/assets/img/pause.svg';
+    } else {
+        audio.pause();
+        playButton.src = './public/assets/img/play.svg';
+    }
+}
+
+// triggered when pressed the play button
+let playButton = document.getElementsByClassName('play-button')[0];
+playButton.addEventListener('click', () => playPause(), false);
+
+// triggered when pressed the space bar
+document.body.onkeyup = function(e){
+    if(e.keyCode == 32){
+        playPause();
+    }
+}
 
 // in some browsers, when a track ends, it doesn't set the audio to paused mode, thats why:
 audio.addEventListener('ended', function(e) {
