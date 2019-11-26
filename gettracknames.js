2dffe728f0fbe67ce3cf608bb477d401432594b4
András Jáky <jakyandras@gmail.com>
2019-11-26 15:27:03 +0100

    ditched the idea to put the function in a separate file

--- gettracknames.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-// requiring path
-const path = require('path');
-
-// requiring fs
-const fs = require('fs');
-const files = fs.readdirSync(path.join(__dirname, 'public/assets/music'));
-console.log(files);
